39d0bc45711e1de1c8044f043e3f7b000ce82f1b
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-06-29 00:38:45 +0530

    add js file

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
-function login(){
+// function login(){
 
-let email = document.getElementById("email").value;
-let password = document.getElementById("password").value;
-let role = document.getElementById("role").value;
+// let email = document.getElementById("email").value;
+// let password = document.getElementById("password").value;
+// let role = document.getElementById("role").value;
 
-if(email == "" || password == "" || role == ""){
-alert("Please fill all fields");
-return;
-}
+// if(email == "" || password == "" || role == ""){
+// alert("Please fill all fields");
+// return;
+// }
 
-if(role == "student"){
-window.location.href = "student_dashboard.html";
-}
+// if(role == "student"){
+// window.location.href = "student_dashboard.html";
+// }
 
-else if(role == "recruiter"){
-window.location.href = "recruiter_dashboard.html";
-}
+// else if(role == "recruiter"){
+// window.location.href = "recruiter_dashboard.html";
+// }
 
-else if(role == "admin"){
-window.location.href = "admin-dashboard.html";
-}
+// else if(role == "admin"){
+// window.location.href = "admin-dashboard.html";
+// }
 
-}
+// }
 
 
 
-// for admin-dashboard
+// // for admin-dashboard
 
-function showSection(section){
+// function showSection(section){
 
-document.getElementById("home").style.display="none";
-document.getElementById("students").style.display="none";
-document.getElementById("recruiters").style.display="none";
-document.getElementById("jobs").style.display="none";
+// document.getElementById("home").style.display="none";
+// document.getElementById("students").style.display="none";
+// document.getElementById("recruiters").style.display="none";
+// document.getElementById("jobs").style.display="none";
 
-document.getElementById(section).style.display="block";
+// document.getElementById(section).style.display="block";
 
-}
+// }
 
-function logout(){
+// function logout(){
 
-window.location.href="index.html";
+// window.location.href="index.html";
 
-}
+// }
+
+
+
